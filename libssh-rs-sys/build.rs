9f9e7149602eb93880a39c0c630a40ff61cf06b2
Wez Furlong <wez@wezfurlong.org>
2026-07-05 07:38:36 +0100

    ci: fix windows link

--- libssh-rs-sys/build.rs
+++ libssh-rs-sys/build.rs
@@ -273,6 +273,7 @@ fn main() {
         println!("cargo:rustc-link-lib=ntdll");
         println!("cargo:rustc-link-lib=iphlpapi");
         println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=advapi32");
     } else {
         println!("cargo:rustc-link-lib=ssl");
         println!("cargo:rustc-link-lib=crypto");
